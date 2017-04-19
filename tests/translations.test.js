cacbfb3e96b560d42a880b58d6e5d5a6eef05d83
Eugene Manuilov <eugene.manuilov@get10up.com>
2017-04-20 00:27:36 +0300

    Added test to check callbacks

--- tests/translations.test.js
+++ tests/translations.test.js
@@ -72,3 +72,36 @@ test('Test ngettext function', () => {
 	const translation = po[sentence][n % 3 == 2 ? 2 : n % 2 == 1 ? 1 : 0];
 	expect(hoc.ngettext(sentence, faker.lorem.sentence(), n)).toBe(translation);
 });
+
+test('Function callbacks for translations and plural forms', () => {
+	const sentence1 = faker.lorem.sentence();
+	const sentence2 = faker.lorem.sentence();
+	const context = faker.lorem.sentence();
+
+	const sentence3 = faker.lorem.sentence();
+	const translation1 = faker.lorem.sentence();
+	const translation2 = faker.lorem.sentence();
+	const translation3 = faker.lorem.sentence();
+
+	const getPO = () => {
+		const po = {};
+
+		po[sentence1] = translation1;
+		po[`${context}\u0004${sentence2}`] = translation2;
+		po[sentence3] = [translation1, translation2, translation3];
+
+		return po;
+	};
+
+	const pluralForm = n => n % 3 == 2 ? 2 : n % 2 == 1 ? 1 : 0;
+
+	const baseComponent = getComponent();
+	const hoc = Textdomain(getPO, pluralForm)(baseComponent);
+
+	expect(hoc.gettext(sentence1)).toBe(translation1);
+	expect(hoc.xgettext(sentence2, context)).toBe(translation2);
+
+	const n = faker.random.number();
+	const translation = getPO()[sentence3][n % 3 == 2 ? 2 : n % 2 == 1 ? 1 : 0];
+	expect(hoc.ngettext(sentence3, faker.lorem.sentence(), n)).toBe(translation);
+})
