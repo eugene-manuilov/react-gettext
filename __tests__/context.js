6b12d758860aaeae5091394c0ed7d906e6d1b1d4
Eugene Manuilov <eugene.manuilov@get10up.com>
2017-04-21 09:31:11 +0300

    Updated context test to check xgettext and ngettext along with gettext function

--- __tests__/context.js
+++ __tests__/context.js
@@ -5,8 +5,6 @@ import renderer from 'react-test-renderer';
 
 import Textdomain from '../lib/index';
 
-const SALT = '7[|UyK-%T`CKw-%j $1/6XTzdd(8+cY,/JCngNVv+wQO6NAv:+^=4GA[,+G@^GI<';
-
 class App extends Component {
 	render() {
 		return <C1 {...this.props} />;
@@ -15,7 +13,22 @@ class App extends Component {
 
 class C1 extends Component {
 	render() {
-		return <span>{this.context.gettext(this.props.text)}</span>;
+		return (
+			<ul>
+				<li>
+					<h1>gettext:</h1>
+					<span>{this.context.gettext(this.props.gettext)}</span>
+				</li>
+				<li>
+					<h1>xgettext:</h1>
+					<span>{this.context.xgettext(this.props.xgettext, this.props.context)}</span>
+				</li>
+				<li>
+					<h1>ngettext:</h1>
+					<span>{this.context.ngettext(this.props.singular, this.props.plural, this.props.num)}</span>
+				</li>
+			</ul>
+		);
 	}
 };
 
@@ -25,14 +38,30 @@ C1.contextTypes = {
 	ngettext: PropTypes.func.isRequired
 };
 
-test('Child component has context functions', () => {
+test('Child component can use context functions', () => {
 	const po = {};
-	const sentence = faker.lorem.sentence();
+	const sentence1 = faker.lorem.sentence();
+	const sentence2 = faker.lorem.sentence();
+	const sentence3 = faker.lorem.sentence();
+	const sentence4 = faker.lorem.sentence();
+	const sentence5 = faker.lorem.sentence();
 
-	po[sentence] = SALT;
+	po[sentence1] = '7[|UyK-%T`CKw-%j $1/6XTzdd(8+cY,/JCngNVv+wQO6NAv:+^=4GA[,+G@^GI<';
+	po[`${sentence3}\u0004${sentence2}`] = 'T6%(h7m%J*O(Bm6!FiRk0 9;V]r`kPz-ROW7E8*t.Aki9}j=hU9xfU8X|.7wEuZ0';
+	po[sentence4] = [
+		'ZgcaUXD7<qhRY#>A~@-`y2%xm@t{TLsN1LvUL`6zamv|e}G|rY`ms-|aMxn#_2f{',
+		'v.,rfN>-w1>j#KBX%eRO[nm@|MgGv,8E.o8-|`t.9Sz{9n#k=+V?W.#k@/tjWq|>'
+	];
 
 	const hoc = Textdomain(po, 'n!=1')(App);
-	const component = renderer.create(React.createElement(hoc, {text: sentence}, []));
+	const component = renderer.create(React.createElement(hoc, {
+		gettext: sentence1,
+		xgettext: sentence2,
+		context: sentence3,
+		singular: sentence4,
+		plural: sentence5,
+		num: 2
+	}, []));
 
 	let tree = component.toJSON();
 	expect(tree).toMatchSnapshot();
