8f0f2e8483e8b908adb290b968e94f0965666c1a
Eugene Manuilov <eugene.manuilov@get10up.com>
2017-06-25 08:32:17 +0300

    Added gettext functions to standalone component

--- src/Textdomain.js
+++ src/Textdomain.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Textdomain extends Component {
 
@@ -9,9 +10,47 @@ class Textdomain extends Component {
 	}
 
 	getPluralForm() {
-		const { pluralForm } = this.props;
+		const { plural } = this.props;
 
-		return pluralForm;
+		return plural;
+	}
+
+	getDelimiter() {
+		return '\u0004'; // End of Transmission (EOT)
+	}
+
+	gettext(message) {
+		const messages = this.getTranslations();
+		return messages.hasOwnProperty(message) ? messages[message] : message;
+	}
+
+	ngettext(singular, plural, n) {
+		const self = this;
+		const messages = self.getTranslations();
+		const pluralIndex = self.getPluralForm(n);
+		const defaultValue = n > 1 ? plural : singular;
+
+		return messages.hasOwnProperty(singular) && Array.isArray(messages[singular]) && messages[singular][pluralIndex]
+			? messages[singular][pluralIndex]
+			: defaultValue;
+	}
+
+	xgettext(message, context) {
+		const self = this;
+		const EOT = self.getDelimiter();
+		const messages = self.getTranslations();
+		const key = context + EOT + message;
+
+		return messages.hasOwnProperty(key) ? messages[key] : message;
+	}
+
+	getChildContext() {
+		const self = this;
+		return {
+			gettext: self.gettext,
+			xgettext: self.xgettext,
+			ngettext: self.ngettext,
+		};
 	}
 
 	render() {
@@ -20,8 +59,15 @@ class Textdomain extends Component {
 
 }
 
-Textdomain.contextTypes = {
+Textdomain.propTypes = {
+	catalog: PropTypes.object,
+	plural: PropTypes.string,
+};
 
+Textdomain.contextTypes = {
+	gettext: PropTypes.func,
+	ngettext: PropTypes.func,
+	xgettext: PropTypes.func,
 };
 
 export default Textdomain;
