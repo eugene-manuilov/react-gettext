53355d7d64be5d30465d393837ce5dba8c8e20f0
Eugene Manuilov <eugene.manuilov@get10up.com>
2017-08-03 14:39:00 +0300

    Compiled translations and connected it to the page component

--- examples/poedit/src/Application.js
+++ examples/poedit/src/Application.js
@@ -9,9 +9,18 @@ class Application extends Component {
 	static renderPage(route) {
 		const { language } = route.match.params;
 
-		console.log(language);
+		let pofile = {
+			catalog: {},
+			plural: 'n != 1',
+		};
 
-		return <Page />;
+		try {
+			pofile = require(`./languages/LC_MESSAGES/${language}.mo.json`);
+		} catch (e) {
+			// do nothing, default pofile will be used
+		}
+
+		return <Page translations={pofile.catalog} plural={pofile.plural} />;
 	}
 
 	render() {
