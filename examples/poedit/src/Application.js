d0a3f798af51836cf110e47f635c54c40be78534
Eugene Manuilov <eugene.manuilov@get10up.com>
2017-08-03 13:54:32 +0300

    Build basic app

---
+++ examples/poedit/src/Application.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Application extends Component {
+
+	componentDidMount() {
+	}
+
+	render() {
+		return (
+			<div>App</div>
+		);
+	}
+
+}
+
+export default Application;
--- examples/poedit/webpack.config.js
+++ examples/poedit/webpack.config.js
@@ -1,7 +1,117 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+const path = require('path');
 
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
+// webpack config
+const config = {};
+
+// devtools
+config.devtool = 'source-map';
+
+// entry and context
+config.context = path.resolve(__dirname, 'src');
+config.entry = {
+	app: './client.js'
+};
+
+// output
+config.output = {
+	path: path.resolve(__dirname, 'build'),
+	publicPath: '/',
+	filename: 'js/[name].js',
+	chunkFilename: '[id].js'
+};
+
+// externals
+config.externals = {
+	firebase: 'firebase'
+};
+
+// define module object
+config.module = {
+	rules: []
+};
+
+// eslint configuration
+config.module.rules.push({
+	test: /\.js$/,
+	enforce: 'pre',
+	exclude: /node_modules/,
+	use: [
+		{
+			loader: 'eslint-loader',
+			options: {
+				failOnWarning: false,
+				failOnError: true
+			}
+		}
+	]
+});
+
+// babel loader rule
+config.module.rules.push({
+	test: /\.js$/,
+	exclude: /node_modules/,
+	use: {
+		loader: 'babel-loader',
+		options: {
+			cacheDirectory: true,
+			presets: [
+				['react'],
+				['env', {targets: {browsers: ["last 2 versions", "safari >= 7"]}}]
+			]
+		}
+	}
+});
+
+// sass loader rule
+config.module.rules.push({
+	test: /\.scss$/,
+	loaders: ExtractTextPlugin.extract({
+		fallback: 'style-loader',
+		use: 'css-loader?sourceMap!postcss-loader?sourceMap!sass-loader?sourceMap'
+	}),
+});
+
+// pug loader rule
+config.module.rules.push({
+	test: /\.(jade|pug)$/,
+	include: path.resolve(__dirname, 'src/html'),
+	loader: 'pug-loader',
+	query: {
+		self: true,
+	},
+});
+
+// define plugins array
+config.plugins = [
+	new webpack.NoEmitOnErrorsPlugin(),
+
+	new HtmlWebpackPlugin({
+		template: path.resolve(__dirname, 'src/html/webpack.pug')
+	}),
+
+	new webpack.optimize.CommonsChunkPlugin({
+		name: 'vendor',
+		filename: 'js/vendor.js',
+		minChunks(module, count) {
+			var context = module.context;
+			return context && context.indexOf('node_modules') >= 0;
+		}
+	}),
+
+	new ExtractTextPlugin({
+		filename: 'styles/[name].css',
+		allChunks: true
+	})
+];
+
+// dev server
+config.devServer = {
+	historyApiFallback: true,
+	contentBase: path.resolve(__dirname, 'build')
+};
+
+module.exports = config;
