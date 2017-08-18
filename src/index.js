cf4f9e57b7de143591324871583d584f4adf2bbf
Eugene Manuilov <eugene.manuilov@get10up.com>
2017-08-18 11:22:51 +0300

    Restructured hoc script

--- src/index.js
+++ src/index.js
@@ -2,25 +2,24 @@ import React from 'react';
 import hoistNonReactStatic from 'hoist-non-react-statics';
 import Textdomain from './Textdomain';
 
-export { Textdomain };
+const withGettext = (translations = {}, pluralForm = 'n != 1') => (WrappedComponent) => {
+	class WithGettext extends Textdomain {
 
-export default function withGettext(translations = {}, pluralForm = 'n != 1') {
-	return (WrappedComponent) => {
-		class WithGettext extends Textdomain {
-
-			render() {
-				return React.createElement(WrappedComponent, this.props);
-			}
-
+		render() {
+			return React.createElement(WrappedComponent, this.props);
 		}
 
-		WithGettext.defaultProps = {
-			translations,
-			plural: pluralForm,
-		};
+	}
 
-		WithGettext.displayName = `withGettext(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
-
-		return hoistNonReactStatic(WithGettext, WrappedComponent);
+	WithGettext.defaultProps = {
+		translations,
+		plural: pluralForm,
 	};
-}
+
+	WithGettext.displayName = `withGettext(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+
+	return hoistNonReactStatic(WithGettext, WrappedComponent);
+};
+
+export { Textdomain };
+export default withGettext;
