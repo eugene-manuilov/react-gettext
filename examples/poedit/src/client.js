4d890361da60efc6072d7ad52f1d573e0bb7ab7a
Eugene Manuilov <eugene.manuilov@gmail.com>
2019-07-16 14:32:56 +0300

    Updated dependencies, reworked build process and removed useless stuff from the example project

--- examples/poedit/src/client.js
+++ examples/poedit/src/client.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
-import './styles/core.scss';
-
 import Application from './Application';
 
 ReactDOM.render(
