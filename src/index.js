5e4cbf18a15b7c967ae0fc0dbe9a3be3a96de368
Eugene Manuilov <eugene.manuilov@gmail.com>
2019-07-16 11:49:24 +0300

    Moved withGettext into separate file

--- src/index.js
+++ src/index.js
@@ -1,36 +1,8 @@
-import React from 'react';
-import hoistNonReactStatic from 'hoist-non-react-statics';
+import withGettext from './withGettext';
 import Textdomain from './Textdomain';
 
-const withGettext = (translations = {}, pluralForm = 'n != 1', options = {}) => (WrappedComponent) => {
-	const args = Object.assign({ withRef: false }, options);
-
-	class WithGettext extends Textdomain {
-
-		getWrappedComponent() {
-			return this.refs.wrappedComponent;
-		}
-
-		render() {
-			const newprops = Object.assign({}, this.props);
-			if (args.withRef) {
-				newprops.ref = 'wrappedComponent';
-			}
-
-			return React.createElement(WrappedComponent, newprops);
-		}
-
-	}
-
-	WithGettext.defaultProps = {
-		translations,
-		plural: pluralForm,
-	};
-
-	WithGettext.displayName = `withGettext(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
-
-	return hoistNonReactStatic(WithGettext, WrappedComponent);
+export {
+	Textdomain,
 };
 
-export { Textdomain };
 export default withGettext;
---
+++ src/withGettext.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import hoistNonReactStatic from 'hoist-non-react-statics';
+import Textdomain from './Textdomain';
+
+const withGettext = (translations = {}, pluralForm = 'n != 1', options = {}) => (WrappedComponent) => {
+	const args = Object.assign({ withRef: false }, options);
+
+	class WithGettext extends Textdomain {
+
+		getWrappedComponent() {
+			return this.refs.wrappedComponent;
+		}
+
+		render() {
+			const newprops = Object.assign({}, this.props);
+			if (args.withRef) {
+				newprops.ref = 'wrappedComponent';
+			}
+
+			return React.createElement(WrappedComponent, newprops);
+		}
+
+	}
+
+	WithGettext.defaultProps = {
+		translations,
+		plural: pluralForm,
+	};
+
+	WithGettext.displayName = `withGettext(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+
+	return hoistNonReactStatic(WithGettext, WrappedComponent);
+};
+
+export default withGettext;
