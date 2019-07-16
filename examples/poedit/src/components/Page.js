d75a9bc9494c71334211ceb5b25b34d81fa1a2bb
Eugene Manuilov <eugene.manuilov@gmail.com>
2019-07-16 14:35:46 +0300

    Updated example project to use the current project as react-gettext dependency

--- examples/poedit/src/components/Page.js
+++ examples/poedit/src/components/Page.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import withGettext from 'react-gettext';
+
+import withGettext from '../../../../src/index.js';
 
 class Page extends Component {
 
