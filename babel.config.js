45ad68ce29ca63dd21e9791ad216eb6a48bb450f
Roberto Alvarez <robertoatc02@gmail.com>
2019-11-21 14:28:12 -0600

    Fixed babel-preset-env browser targets

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,9 @@
+const BABEL_ENV = process.env.BABEL_ENV;
+const targets = BABEL_ENV === 'test' ? { node: 6 } : { browsers: 'defaults' };
+
 module.exports = {
 	presets: [
-		[
-			'@babel/preset-env',
-			{
-				targets: {
-					node: 6,
-				},
-			},
-		],
+		['@babel/preset-env', { targets } ],
 		'@babel/preset-react',
 	],
 };
